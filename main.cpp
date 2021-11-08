fee74c8193940f8c15d67f2e394eb8c482da42d3
Tudor Deviza <tudor.devizamd@gmail.com>
2021-11-08 18:51:43 +0200

    Comments

--- main.cpp
+++ main.cpp
@@ -10,20 +10,20 @@ int main() {
     //creare cleint
     Client *c1 = new Client("Tudor", new Cont(120));
     b->addClient(c1); // adugare client
-    b->show_clients();
+    b->show_clients(); // afiseasa clienti
 
     //
-    b->giveCredit(0, new Credit(1000, 25, "21.01.2021"));
-    b->setDepozit(0, new Depozit(1200, 1.25, "21.11.2021"));
+    b->giveCredit(0, new Credit(1000, 25, "21.01.2021")); // adauga credit la client cu indexul 0
+    b->setDepozit(0, new Depozit(1200, 1.25, "21.11.2021"));// adauga depozit la client cu indexul 0
 
 
-    b->getClietByIndex(0)->getCont().showCredits();
-    b->getClietByIndex(0)->getCont().showDepozit();
+    b->getClietByIndex(0)->getCont().showCredits(); //  afisesaza creditele clientului cu indexul 0
+    b->getClietByIndex(0)->getCont().showDepozit();// afisesaza depozitele clientului cu indexul 0
 
-    b->getClietByIndex(0)->getCont().getDepozitByIndex(0)->getMonney();
-    b->getClietByIndex(0)->getCont().setMoneyForCredit(0, 1800);
+    b->getClietByIndex(0)->getCont().getDepozitByIndex(0)->getMonney(); // primeste procentle de la depozit
+    b->getClietByIndex(0)->getCont().setMoneyForCredit(0, 1800); // achita pentru un anumit credit
 
-    cout << "Suma pe cont " << b->getClietByIndex(0)->getCont().getSuma();
+    cout << "Suma pe cont " << b->getClietByIndex(0)->getCont().getSuma(); // afiseaza suma curenta de pe cont a clientului
 
     return 0;
 }
